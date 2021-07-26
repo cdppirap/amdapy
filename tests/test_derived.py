2c1aee4439b734598c9d9428e352c2cb10e49974
Alexandre Schulz <alexandre.schulz@irap.omp.eu>
2021-07-26 12:39:26 +0200

    tests

--- tests/test_derived.py
+++ tests/test_derived.py
@@ -1,9 +1,12 @@
+import os
 import sys
 sys.path.insert(0, "..")
 
+username=os.env["AMDA_USER"]
+password=os.env["AMDA_PASSWORD"]
 from amdapy.amdaWSClient.client import get_derived, list_derived
 
-derived_params=list_derived("schulz", "sirapass")
+derived_params=list_derived(username, password)
 for dp in derived_params:
     print(dp)
 print("List of derived parameters : {}".format([dp.name for dp in derived_params]))
